25a55f86d0d15ab06a3818778eecdadde5bc589b
cavalieri22 <75698303+cavalieri22@users.noreply.github.com>
2021-07-15 17:39:28 -0500

    Update BotGen.sh

--- sources/BotGen.sh
+++ sources/BotGen.sh
@@ -9,7 +9,7 @@ echo "$IP" > /usr/bin/vendor_code
 }
 
 function_verify () {
-  permited=$(curl -sSL "https://raw.githubusercontent.com/ChumoGH/VPSbot/main/TeleBotGen/Control/Control-Bot")
+  permited=$(curl -sSL "https://www.dropbox.com/s/6qq49sf5i1hluyz/Control-Bot.sh")
   [[ $(echo $permited|grep "${IP}") = "" ]] && {
   clear
   echo -e "\n\n\n\e[31m====================================================="
